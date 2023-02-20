77029a9086fe4cfcd5c2667c7953ba16de8927ca
Christopher Terrano <mterrano1@yahoo.com>
2023-02-20 01:29:47 -0600

    create submodules

---
+++ terraform/terraform.tf
@@ -0,0 +1,21 @@
+terraform {
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.47.0"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0.4"
+    }
+
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "~> 2.2.0"
+    }
+  }
+
+  required_version = "~> 1.3"
+}
